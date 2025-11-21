441c05855e805f39dec414ff160fa926991c5f9f
OmarAbdulqader1 <omer.abdulaziz05@gmail.com>
2025-11-21 14:44:30 +0000

    Add files via upload

---
+++ LibrarySimulator.java
@@ -0,0 +1,140 @@
+
+
+/* 1- Omar Abdulgader 446101952
+ * 
+ * 2- Fahad Alboqami 446103819
+ * 
+ * 3- Saud AlSayyari 446102327
+ */
+
+import java.util.Scanner;
+
+public class LibrarySimulator {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		//predefined library accounts
+		Member Account1 = new Member (111, "Account one", 0);
+		Member Account2 = new Member (222, "Account two", 0);
+		Member Account3 = new Member (333, "Account three", 0);
+		
+		
+		
+		//the library:
+		while(true) {
+			System.out.println("*Welcome to the Library!*");
+			System.out.println();
+			System.out.println("Please choose an option:");
+			System.out.println("1- login as " + Account1.getname() + " ID number:" + Account1.getid());
+			System.out.println("2- login as " + Account2.getname() + " ID number:" + Account2.getid());
+			System.out.println("3- login as " + Account3.getname() + " ID number:" + Account3.getid());
+			System.out.println("4- login as administrator ");
+			System.out.println("5- Exit the program");
+			System.out.println("Enter you're pick: ");
+			
+			int pick = input.nextInt(); //the user's pick
+			input.nextLine();
+			
+			
+			if(pick >= 1 && pick <= 3) {
+				Member account;
+				if(pick == 1)
+					account = Account1;
+				else if(pick == 2)
+					account = Account2;
+				else
+					account = Account3;
+				
+				boolean Session_Running = true;
+				
+				while (Session_Running) {
+					System.out.println("\nWhat would you like to do? :");
+					System.out.println("1- View you're Borrowed Books");
+					System.out.println("2- Borrow a book");
+					System.out.println("3- Return a book");
+					System.out.println("4- view session summary");
+					System.out.println("5- Exit to main menu");
+					System.out.println("Enter you're pick: ");
+					
+				int pick2 = input.nextInt();
+				    input.nextLine();
+				    
+				    switch(pick2) {
+				    case 1:
+				    	account.viewBorrowedCount();
+				    	break;
+				    case 2:
+				    	account.borrowOne();
+				    	break;
+				    case 3:
+				    	account.returnOne();
+				    	break;
+				    case 4:
+				    	account.displayStatistics();
+				    	break;
+				    case 5:    //to exit to main menu
+				    	System.out.println("session ended. Currently have " + account.getBorrowedCount() + " books");
+				    	Session_Running = false;
+				    	break;
+				    default :
+				    	System.out.println("invalid option.");
+				    	break;
+				    }
+				}
+			}else if(pick == 4) {
+			
+					boolean admin = true;
+					while (admin) {
+						System.out.println();
+						System.out.println("What would you like to do?");
+						System.out.println("1- View Total Revenue");
+						System.out.println("2- View Most Frequent Operation");
+						System.out.println("3- Exit to main menu");
+						
+						int pick3 = input.nextInt();
+						input.nextLine();
+						
+						switch (pick3) {
+						case 1:
+							//Total Revenue
+							System.out.printf("The Total Revenue is: %.2f\n" , Member.TotalRevenue );
+							break;
+						case 2:
+							//comparing between the operations
+							if (Member.TotalBorrows > Member.TotalReturns) {
+								System.out.println("the most frequent operation is: Borrows");
+							} else if (Member.TotalReturns > Member.TotalBorrows) {
+								System.out.println("the most frequent operation is: Returns");
+							} else if (Member.TotalReturns == 0 && Member.TotalBorrows==0) {
+								System.out.println("There are no operations yet");
+							} else {
+								System.out.println("Borrows and Returns are Tied");
+							}
+							break;
+						case 3:
+							//returning to main menu
+							admin = false;
+							break;
+							
+							default:
+								System.out.println("invalid option.");
+								System.out.println();
+								break;
+						}
+								
+							}
+						
+				    //exiting the program
+					} else if (pick == 5) {
+						System.out.println("Goodbye!!");
+						break; //break out of the loop
+					} else {
+						System.out.println("Invalid option.");
+				}
+				
+			}
+		input.close();
+		}
+		
+	}
+
---
+++ Member.java
@@ -0,0 +1,116 @@
+
+public class Member {
+	
+	/* 1- Omar Abdulgader 446101952
+	 * 
+	 * 2- Fahad Alboqami 446103819
+	 * 
+	 * 3- Saud AlSayyari 446102327
+	 */
+	
+ //instance attributes
+ private int id;
+ private String name;
+ private int borrowedCount;
+ 
+
+ private int numViewBorrowed;
+ private int numBorrows;
+ private int numReturns;
+ private double sessionFees;
+ 
+ 
+ public static double TotalRevenue;
+ public static int TotalViewBorrowed;
+ public static int TotalBorrows;
+ public static int TotalReturns;
+ 
+ //Methods
+ 
+ //Constructor
+ public Member(int id, String name, int borrowedCount) {
+	 this.id=id;
+	 this.name=name;
+	 this.borrowedCount=borrowedCount;
+ }
+ 
+ private boolean canBorrow() {
+	 return borrowedCount < 5;  //cannot borrow more than 5 books
+ }
+ private boolean canReturn() {
+	 return borrowedCount > 0; //can return only when there are books
+ }
+ 
+//view borrowed books
+ public void viewBorrowedCount() {              
+	 System.out.println("You currently have: " + borrowedCount + " Book(s)");
+	 System.out.println();
+	 numViewBorrowed++;
+	 TotalViewBorrowed++;
+ }
+ //borrowing a book
+ public boolean borrowOne() {                 
+	 if(canBorrow()) {             //if books < 5
+		 System.out.println("borrowed 1 book. Fee = 0.50");
+			System.out.println();
+		borrowedCount++;
+		numBorrows++;
+		TotalBorrows++;
+		sessionFees+=0.50;
+		TotalRevenue+=0.50;
+		
+		return true;
+	 }
+	 else {                      //if books >= 5
+		 System.out.println("you can't borrow more than 5 books");
+	        System.out.println();
+		 return false;
+	 }
+	
+ }
+ //returning a book
+ public boolean returnOne() {   
+	 if (canReturn()) {         //if books > 0
+		 System.out.println("you returned 1 book successfully");
+			System.out.println();
+		borrowedCount--;
+		numReturns++;
+		TotalReturns++;
+		return true;
+	 }
+	 else {                    //if books <= 0
+		 System.out.println("you have no books to return");
+		    System.out.println();
+		return false;
+	 }
+ }
+ //prints session's statistics
+ public void displayStatistics() {
+	 System.out.println();
+	 System.out.println("Session Summary:");
+	 System.out.println();
+	 System.out.println("You have borrowed " + numBorrows + " Book(s)");
+	 System.out.println("You have returned " + numReturns + " Book(s)");
+	 System.out.println("You have viewed you're books " + numViewBorrowed + " times");
+	 System.out.println("You're Fees: " + sessionFees);
+	 System.out.println();
+ }
+ //resets session statistics
+ public void reset() {
+	 numViewBorrowed=0;
+	 numBorrows=0;
+	 numReturns=0;
+	 sessionFees=0;
+ }
+ public String getname() {
+	 return name;
+ }
+ public int getid() {
+	 return id;
+ }
+ public int getBorrowedCount() {
+	 return borrowedCount;
+ }
+ 
+ 
+}
